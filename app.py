73242e7300d409d68ade2e6fe062c086a949c263
Chris Morroni <chmorroni@gmail.com>
2020-06-08 16:51:24 -0600

    Add email notifications when humidity or temperature is out of range

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ import sys
 import threading
 import time
 
+import lib.Accounts as Accounts
 from lib.Buffer import CircBuf
+from lib.Email import Email
 
 if (len(sys.argv) > 1) and (sys.argv[1] == "-test"):
     print("Using mock I/O")
@@ -27,12 +29,18 @@ else:
 
 TEMPERATURE_CEIL_F = 36
 TEMPERATURE_FLOOR_F = 34
+TEMPERATURE_WARNING_UPPER_F = 40
+TEMPERATURE_WARNING_LOWER_F = 30
 
 HUMIDITY_TARGET_PERCENT = 80
+HUMIDITY_WARNING_UPPER_PERCENT = 100
+HUMIDITY_WARNING_LOWER_PERCENT = 50
 ATOMIZER_RUN_DELAY_S = 5
 
 SAMPLE_FREQ_HZ = 1
 SAMPLE_BUFFER_LEN = 24 * 60 * 60 * SAMPLE_FREQ_HZ # store 24 hours worth of data
+
+MIN_EMAIL_PERIOD_S = 15 * 60 # 15 minutes
 
 
 buffer = CircBuf(SAMPLE_BUFFER_LEN)
@@ -44,6 +52,7 @@ compressor_pins = [2, 3, 4]
 compressor = Compressor(compressor_pins)
 door = DoorSwitch(1)
 # atomizer = WaterAtomizer(3) # TODO actual pin
+email = Email(MIN_EMAIL_PERIOD_S)
 
 
 def sample_periodic():
@@ -71,6 +80,12 @@ def sample_periodic():
 
     # if (humidity < HUMIDITY_TARGET_PERCENT) and (atomizer.time_since_last_run() > ATOMIZER_RUN_DELAY_S):
         # atomizer.run()
+
+    # check warning bounds
+    if (temp > TEMPERATURE_WARNING_UPPER_F) or (temp < TEMPERATURE_WARNING_LOWER_F):
+        email.send_mail(Accounts.TO_EMAIL, "Refrigerator Temperature Out of Bounds", "Current temperature {:.3f} F".format(temp))
+    if (humidity > HUMIDITY_WARNING_UPPER_PERCENT) or (humidity < HUMIDITY_WARNING_LOWER_PERCENT):
+        email.send_mail(Accounts.TO_EMAIL, "Refrigerator Humidity Out of Bounds", "Current humidity {:.2f}%".format(humidity))
 
     # schedule next sampling
     timer = threading.Timer(1 / SAMPLE_FREQ_HZ, sample_periodic)
---
+++ lib/Email.py
@@ -0,0 +1,46 @@
+
+from datetime import datetime
+import lib.Accounts as Accounts
+import smtplib
+import time
+
+class Email:
+    def __init__(self, min_send_period_s):
+        self.min_send_period_s = min_send_period_s
+        self.last_run = datetime(2000, 1, 1, 0, 0, 0)
+
+    def send_mail(self, recipient, subject, content):
+        # abort if we sent an email too recently
+        if (datetime.now() - self.last_run) < self.min_send_period_s:
+            return
+        
+        self.last_run = datetime.now()
+
+        # create headers
+        headers = ["From: " + Accounts.FROM_USERNAME, "Subject: " + subject, "To: " + recipient,
+                   "MIME-Version: 1.0", "Content-Type: text/html"]
+        headers = "\r\n".join(headers)
+
+        # connect to server
+        session = smtplib.SMTP(Accounts.SMTP_SERVER, Accounts.SMTP_PORT)
+        session.ehlo()
+        session.starttls()
+        session.ehlo()
+
+        # login
+        session.login(Accounts.FROM_USERNAME, Accounts.FROM_PASSWORD)
+
+        # send email and disconnect
+        session.sendmail(Accounts.FROM_USERNAME, recipient, headers + "\r\n\r\n" + content)
+        session.quit
+
+if __name__ == "__main__":
+    sender = Email(5)
+
+    sendTo = Accounts.TO_EMAIL
+    emailSubject = "Hello World"
+    emailContent = "This is a test"
+
+    while True:
+        sender.send_mail(sendTo, emailSubject, emailContent)
+        time.sleep(1)
